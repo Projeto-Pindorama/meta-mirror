6219bb8b253806dfba70d16f0dfae9e80408123e
Luiz Antônio <luiz.antonio.rangel@bol.com.br>
2021-01-15 09:44:37 -0300

    Add files.

---
+++ scripts/mk_serverinfo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Simple Shell Script to generate the SERVERINFO.txt that is displayed when otto(8) connects to the server and sync the metapackage
+# It shall be on the website root (ex: example.com/SERVERINFO.txt), unless you create a subdomain that still having the directory
+# organization/hierarchy that an Otto mirror would usually have (ex: otto.example.com/SERVERINFO.txt).i
+# You can personalize it (both the message and script) without any trouble.
+# License: Volkerdings-Slackware (or Public Domain).
+
+ipaddr=`curl -s http://ipinfo.io/ip`
+total_memory="$[`sed 1q /proc/meminfo | awk '{print $2}'` / 1000]"
+cpu_name="`cat /proc/cpuinfo | grep 'model name' | cut -f 2 -d ":" | awk '{$1=$1}1'`"
+cpu_cores="`echo $cpu_name | wc -l `"
+country_name="`curl -s https://ipapi.co/${ipaddr}/country_name`"
+city_name="`curl -s https://ipapi.co/${ipaddr}/city/`"
+latlong="`curl -s https://ipapi.co/${ipaddr}/latlong/`"
+
+cat > SERVERINFO.txt <<-EOD
+Server IP Address:  ${ipaddr}
+Server hardware:    ${cpu_cores}x `printf '%s' "${cpu_name}" | uniq` with ${total_memory}MB
+Server hostname:    `uname -n`
+Server geolocation: ${city_name}, ${country_name}; ${latlong}
+Webmaster contact:  ${EMAIL}
+EOD
